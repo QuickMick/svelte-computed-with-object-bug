81a81b3920ad69eb49934d2dd7f3c6cd687f861f
QuickMick <figura.m@web.de>
2022-02-17 17:31:12 +0100

    added another testcase

--- app.svelte
+++ app.svelte
@@ -1,15 +1,16 @@
 <script>
   import TextField from "./text-field.svelte";
   import TextFieldWithString from "./text-field-with-string.svelte";
-  let valueAsObject = { s: "" };
+  import TextFieldWithUpdateFunction from "./text-field-with-update-function.svelte";
 
   let valueAsString = "";
+  let valueAsObjectButBindString = { s: "" };
 
+  let valueAsObject = { s: "" };
   let valueAsObjectButBind = { s: "" };
 </script>
 
 <div class="main">
-
   <!-- example that works like i would expect -->
   <h3>This works (bind string)</h3>
   <div>
@@ -20,8 +21,8 @@
   <!-- example that works also like i would expect -->
   <h3>This works as well (bind string of object)</h3>
   <div>
-    <TextFieldWithString bind:value={valueAsObjectButBind.s} />
-    {valueAsObjectButBind.s}
+    <TextFieldWithString bind:value={valueAsObjectButBindString.s} />
+    {valueAsObjectButBindString.s}
   </div>
 
   <!-- example that does basically the same, but does not work as i would expect -->
@@ -33,7 +34,12 @@
     <TextField value={valueAsObject} />
     {valueAsObject.s}
   </div>
-  
+
+  <h3>This works (bind object, use update-function in computed)</h3>
+  <div>
+    <TextFieldWithUpdateFunction bind:value={valueAsObjectButBind} />
+    {valueAsObjectButBind.s}
+  </div>
 </div>
 
 <style>
---
+++ text-field-with-update-function.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let value = "";
+  let inputText = value;
+
+  $: {
+    // PROBLEM: this is updated on every keytroke,
+    // regardles wheather value or value.s actually has changed or hasn't
+    console.log("trigger text-with-object-and-bind update");
+    update(value);
+  }
+
+  function update(d) {
+    inputText = d.s;
+    value = value;
+  }
+
+  function applyUpdate() {
+    value.s = inputText;
+  }
+</script>
+
+<textarea
+  maxlength={100}
+  on:blur={applyUpdate}
+  class="text-input acknowledge"
+  placeholder="No Description"
+  bind:value={inputText}
+/>
